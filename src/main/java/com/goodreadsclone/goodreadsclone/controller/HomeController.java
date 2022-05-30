29987af0ec0953cd3b95c6aad25aaaaf19cc370e
mainalito <81359755+mainalito@users.noreply.github.com>
2022-05-30 10:47:21 +0300

    github oauth security

--- src/main/java/com/goodreadsclone/goodreadsclone/controller/HomeController.java
+++ src/main/java/com/goodreadsclone/goodreadsclone/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.goodreadsclone.goodreadsclone.entities.BookRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
@@ -12,6 +13,10 @@ public class HomeController {
     public String home(){
         //bookRepository.findAllById(id, pageable)
         return "home";
+    }
+    @GetMapping("/style.css")
+    public String falseLoginRedirect(){
+        return "redirect:/search";
     }
     
 }
