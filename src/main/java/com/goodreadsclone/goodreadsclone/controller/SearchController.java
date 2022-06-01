bb08f8f9c0c0e152d9bd9d7cbf2d534f738c35de
mainalito <81359755+mainalito@users.noreply.github.com>
2022-06-01 20:00:40 +0300

    added book-tracker features and css

--- src/main/java/com/goodreadsclone/goodreadsclone/controller/SearchController.java
+++ src/main/java/com/goodreadsclone/goodreadsclone/controller/SearchController.java
@@ -1,5 +1,6 @@
 package com.goodreadsclone.goodreadsclone.controller;
 
+import com.goodreadsclone.goodreadsclone.entities.Book;
 import com.goodreadsclone.goodreadsclone.models.book;
 import com.goodreadsclone.goodreadsclone.models.docs;
 import com.goodreadsclone.goodreadsclone.models.publisher;
@@ -37,17 +38,11 @@ public class SearchController {
                 .baseUrl(SEARCH_URL).build();
 
     }
-    
 
-    // @GetMapping("/")
-    // public String index() {
-    //     return "index";
-    // }
- 
 
     @GetMapping("/search")
-    public String fetchBook(@RequestParam (value = "q", defaultValue = "Think and grow rich", required = false) String q, Model model) {
-  
+    public String fetchBook(@RequestParam (value = "q", defaultValue = "", required = false) String q, Model model) {
+        Book bookUserBook = new Book();
         Mono<book> bookMono = this.webclient.get().uri(UriBuilder -> UriBuilder.queryParam("q", q.replace(" ", "+"))
                 .build()).retrieve().bodyToMono(book.class);
         book book = bookMono.block();
@@ -62,6 +57,7 @@ public class SearchController {
             }
             bookResult.setUrl(BOOK_URL + bookResult.getKey() + ".json");
             bookResult.setCover_i(cover_id);
+            
         }).collect(Collectors.toList()) ;
 
         
